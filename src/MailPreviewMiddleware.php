ee0ffa443b416e4f25ddecb85c3c558f7e4eb9a9
Mark <mark@digifactory.nl>
2019-03-20 12:15:03 +0100

    Fix for https://github.com/themsaid/laravel-mail-preview/issues/45

--- src/MailPreviewMiddleware.php
+++ src/MailPreviewMiddleware.php
@@ -16,16 +16,24 @@ class MailPreviewMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        $addLinktoResponse = false;
 
         if (
             $request->hasSession() &&
-            $response instanceOf Response &&
             $previewPath = $request->session()->get('mail_preview_path')
         ) {
-            $this->addLinkToResponse($response, $previewPath);
+            $addLinktoResponse = true;
 
             $request->session()->forget('mail_preview_path');
+        }
+
+        $response = $next($request);
+
+        if (
+            $response instanceOf Response &&
+            $addLinktoResponse
+        ) {
+            $this->addLinkToResponse($response, $previewPath);
         }
 
         return $response;
