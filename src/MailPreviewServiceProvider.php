b8fe1b04e431c20626f9fd5abcf68856f468a3ed
freekmurze <freekmurze@users.noreply.github.com>
2021-01-27 22:26:01 +0000

    Fix styling

--- src/MailPreviewServiceProvider.php
+++ src/MailPreviewServiceProvider.php
@@ -58,7 +58,7 @@ class MailPreviewServiceProvider extends PackageServiceProvider
 
     protected function registerRouteMacro(): self
     {
-        Route::macro('mailPreview', function(string $prefix = 'spatie-mail-preview') {
+        Route::macro('mailPreview', function (string $prefix = 'spatie-mail-preview') {
             Route::get($prefix)->middleware(StartSession::class)->name('mail.preview');
         });
 
